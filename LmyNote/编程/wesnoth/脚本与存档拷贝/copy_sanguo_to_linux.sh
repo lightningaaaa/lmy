d1405189fa8a2b83ee83d467132863306bd279bf
kooper <563413639@qq.com>
2020-02-07 23:40:59 +0800

    shell

---
+++ LmyNote/编程/wesnoth/脚本与存档拷贝/copy_sanguo_to_linux.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp /mnt/hgfs/MyCode_ShareVm/sanguo.cfg  /home/kooper/.local/share/wesnoth/1.14/editor/scenarios/
---
+++ LmyNote/编程/wesnoth/脚本与存档拷贝/copy_sanguo_to_windows.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp /home/kooper/.local/share/wesnoth/1.14/editor/scenarios/sanguo.cfg /mnt/hgfs/MyCode_ShareVm/sanguo.cfg
