6d44ced9e7b244c0f3830fba47ae57d173071c05
lightningaaaa <563413639@qq.com>
2020-12-23 22:28:18 +0800

    test

--- test/main.c
+++ test/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main()
 {
 	printf("ok\n");
+	int fd = open("./main.c", O_RDWR);
+	int fd1 = open("./main.c", O_RDWR);
+	printf("fd=%d,fd1=%d\n", fd, fd1);
 	return 0;
 }
